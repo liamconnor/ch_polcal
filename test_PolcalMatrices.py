28d011b3b52372e2eb1ee82435ef17313a33c6a1
Liam Connor <connor@muskrat.cita.utoronto.ca>
2014-12-03 11:44:29 -0500

    Changed it to inherit from class it is testing

--- test_PolcalMatrices.py
+++ test_PolcalMatrices.py
@@ -4,16 +4,15 @@ import numpy.linalg as la
 import polcalqui as pq
 import PolcalMatrices as P
 
-class TestPolcalMat:
+class TestPolcalMat(P.PolcalMatrices):
 	
-	PM = P.PolcalMatrices([1,1,1,1])
 
 	def test_wq_special(self):
 		""" Confirm that wq has determinent 1 
 			for random angles
 		"""
 		angles = np.random.rand(3)
-		wq = PM.wq(angles[0], angles[1], angles[2])
+		wq = self.wq(angles[0], angles[1], angles[2])
 
 		assert np.round(la.det(wq), 5) == 1 
 
@@ -21,8 +20,8 @@ class TestPolcalMat:
 		""" Confirm that wq is unitary
 		"""
 		angles = np.random.rand(3)
-		wq = PM.wq(angles[0], angles[1], angles[2])
-		dq = PM.dq()
+		wq = self.wq(angles[0], angles[1], angles[2])
+		dq = self.dq
 
 		assert (np.round(wq * dq * wq.H) == dq).all()
 	
@@ -30,7 +29,7 @@ class TestPolcalMat:
 		""" Check M_U's determinent
 		"""
 		angles = np.random.rand(3)
-		mu = PM.mu(angles[0], angles[1], angles[2])
+		mu = self.mu(angles[0], angles[1], angles[2])
 
 		assert np.round(la.det(mu), 3) == -1
 
@@ -38,7 +37,7 @@ class TestPolcalMat:
 		""" Check M_IC's determinent
 		"""
 		angles = np.random.rand(3)
-		mic = PM.mic(angles[0], angles[1], angles[2])
+		mic = self.mic(angles[0], angles[1], angles[2])
 
-		assert 	np.round(la.det(mic), 3) == PM.I**2 - PM.C**2
+		assert 	np.round(la.det(mic), 3) == self.I**2 - self.C**2
 
